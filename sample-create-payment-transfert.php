0c77c7e13d2917a8cc616e1c87395b04e2d513c2
Richardson <37945903+R1997080620@users.noreply.github.com>
2021-12-16 11:28:13 -0500

    Update sample-create-payment-transfert.php

--- sample-create-payment-transfert.php
+++ sample-create-payment-transfert.php
@@ -1,35 +1,5 @@
 <?php
-// Sample Authentication Basic
-
-/**
- * Client Id: 60cac971ad41e99724d2e11a83eaebec
- * Client secret: 6aXNmZChWgLDySQ3mz1EPDIzMkTejWwb_s7orVYUiWD4Ln2rl7OR8HtBlnVqYJhO
- * HOST_REST_API for test : sandbox.moncashbutton.digicelgroup.com/Api
- * HOST_REST_API for live : moncashbutton.digicelgroup.com/Api
- */
-
-$auth_moncash = 'https://60cac971ad41e99724d2e11a83eaebec:6aXNmZChWgLDySQ3mz1EPDIzMkTejWwb_s7orVYUiWD4Ln2rl7OR8HtBlnVqYJhO@sandbox.moncashbutton.digicelgroup.com/Api/oauth/token';
-
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, $auth_moncash);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "scope=read,write&grant_type=client_credentials");
-
-$headers = array();
-$headers[] = 'Accept: application/json';
-$headers[] = 'Content-Type: application/x-www-form-urlencoded';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close($ch);
-
-$result = json_decode($result, true);
-/** END */
+require_once 'controller/auth.php';
 
 /**
  * Sample Create Payment Transfert  
@@ -39,7 +9,7 @@ $path = 'https://sandbox.moncashbutton.digicelgroup.com/Api/v1/Transfert';
 
 $data = json_encode([
     "amount"  => 100,
-    "receiver"  => 50937007294,
+    "receiver"  => 36043882,
     "desc" => 'Hello word'
 ]);
 
